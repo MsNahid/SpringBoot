25d015159d420e85dae059da209bc60af98fc4db
MsNahid <m.nahidzaman@gmail.com>
2022-05-14 13:59:11 +0600

    inject from properties

--- SpringBootDemo/src/main/java/com/nahidsohel/springbootdemo/restcontroller/FunRestController.java
+++ SpringBootDemo/src/main/java/com/nahidsohel/springbootdemo/restcontroller/FunRestController.java
@@ -1,11 +1,17 @@
 package com.nahidsohel.springbootdemo.restcontroller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
+    @Value("${team.name}")
+    private String teamName;
+
+    @Value("${coach.name}")
+    private String coachName;
     @GetMapping("/")
     public String getHelloWorld(){
         return "Hello World..Hi everyone";
@@ -14,5 +20,10 @@ public class FunRestController {
     @GetMapping("/fortune")
     public String getFortune(){
         return "Open the magic box please..";
+    }
+
+    @GetMapping("/teamInfo")
+    public String teamInfo(){
+        return "Coach: " + coachName + ", " +  "Team: " + teamName + ".\n";
     }
 }
